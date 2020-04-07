216f6636dc001c1cfa07ed251317e7b890ccedd1
Thiago P <thiagopmaceda@gmail.com>
2020-04-07 15:44:43 -0300

    Adicionado componente de profile na sidebar

--- src/components/Sidebar/Profile/index.tsx
+++ src/components/Sidebar/Profile/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {
+  SidebarTitle,
+  SidebarContent,
+  SidebarLineThrough,
+} from '../utils/SidebarUtils';
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <>
+      <SidebarTitle>Profile</SidebarTitle>
+      <SidebarLineThrough />
+      <SidebarContent>
+        27-year old web developer from Colorado Springs With 5+ years of work
+        experience in various fields.
+      </SidebarContent>
+    </>
+  );
 };
 
 export default Profile;
