e4f7fa2f7a61eae02c0c0b95e4556fc871907e92
Thiago P <thiagopmaceda@gmail.com>
2020-04-07 15:44:22 -0300

    Adicionado componente de contato na sidebar

--- src/components/Sidebar/Contact/index.tsx
+++ src/components/Sidebar/Contact/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  SidebarTitle,
+  SidebarContent,
+  SidebarLineThrough,
+} from '../utils/SidebarUtils';
 
 const Contact = () => {
-  return <h1>Contact</h1>;
+  return (
+    <>
+      <SidebarTitle>Contact</SidebarTitle>
+      <SidebarLineThrough />
+      <SidebarContent>
+        123-456-7890 <br /> 012-345-6789
+      </SidebarContent>
+
+      <SidebarContent>
+        1490 General Woods <br /> Colorado Springs
+      </SidebarContent>
+
+      <SidebarContent>
+        brianwalker.co <br /> mail@brianwalker.com
+      </SidebarContent>
+    </>
+  );
 };
 
 export default Contact;
