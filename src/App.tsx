16324610dc0ad14a2cc1a1d05ddc93db2ffffbec
Thiago P <thiagopmaceda@gmail.com>
2020-04-07 11:08:44 -0300

    adicionada fontes montserrat

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import WorkExperience from './components/WorkExperience';
 import Education from './components/Education';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Sidebar />
       <WorkExperience />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  background-color: #333333;
+  display: flex;
+  height: 50vh;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const HeaderImg = styled.img`
+  margin-bottom: 15%;
+`;
+
+const HeaderDevName = styled.h1`
+  color: white;
+  text-align: center;
+  letter-spacing: 10px;
+`;
+
+const HeaderDevRole = styled.p`
+  color: white;
+`;
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <HeaderContainer>
+      <HeaderImg src='https://raw.githubusercontent.com/b2w-marketplace/code-challenge/master/files/avatar-dev.png'></HeaderImg>
+      <HeaderDevName>
+        BRIAN <br /> WALKER
+      </HeaderDevName>
+      <HeaderDevRole>Web Developer</HeaderDevRole>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
