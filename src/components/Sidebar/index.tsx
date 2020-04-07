5520347f9c911107a30ee580d68a39363334d385
Thiago P <thiagopmaceda@gmail.com>
2020-04-07 15:43:51 -0300

    Adicionado arquivo para renderizar os componentes da sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import Skills from './Skills';
 import Profile from './Profile';
 import Contact from './Contact';
+import styled from 'styled-components';
+
+const SidebarContainer = styled.div`
+  background-color: #333333;
+  display: flex;
+  margin-top: 5px;
+  flex-direction: column;
+  text-align: center;
+  padding-bottom: 80px;
+`;
 
 const Sidebar = () => {
   return (
-    <>
+    <SidebarContainer>
       <Profile />
       <Contact />
       <Skills />
-    </>
+    </SidebarContainer>
   );
 };
 
