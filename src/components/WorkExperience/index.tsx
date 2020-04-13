a48b6f5c6a8499d145a9eb37b82b64a2f218b596
Thiago P <thiagopmaceda@gmail.com>
2020-04-13 15:46:00 -0300

    Importando os styled-components que são compartilhados entre o Education e o WorkExperience.

--- src/components/WorkExperience/index.tsx
+++ src/components/WorkExperience/index.tsx
@@ -1,91 +1,47 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const WorkExperienceTitle = styled.h2`
-  text-transform: uppercase;
-  color: #000;
-`;
-
-const WorkExperienceLineThrough = styled.hr`
-  width: 90%;
-  height: 1px;
-  background-color: #000;
-  display: block;
-  border: 0;
-  border-top: 1px solid #000;
-  margin: 0;
-  padding: 0;
-
-  @media (min-width: 1440px) {
-    width: 80%;
-  }
-`;
-
-const WorkExperienceRole = styled.h3`
-  text-transform: uppercase;
-  color: #000;
-`;
-
-const WorkExperienceParagraph = styled.p`
-  color: #000;
-  line-height: 30px;
-  font-weight: bold;
-
-  @media (min-width: 1440px) {
-    width: 70%;
-  }
-`;
-
-const WorkExperienceItemContainer = styled.div`
-  margin-bottom: 50px;
-
-  @media (min-width: 1024px) {
-    width: 450px;
-  }
-
-  @media (min-width: 1440px) {
-    margin-left: 70px;
-    width: 100%;
-  }
-`;
+import LineThrough from '../LineThrough';
+import Title from '../Title';
+import Role from '../Role';
+import Paragraph from '../Paragraph';
+import ItemContainer from '../ItemContainer';
 
 const WorkExperience = () => {
   return (
     <div>
-      <WorkExperienceItemContainer>
-        <WorkExperienceTitle>Work Experience</WorkExperienceTitle>
-        <WorkExperienceLineThrough />
-        <WorkExperienceRole>Front End Developer @ Hillson</WorkExperienceRole>
-        <WorkExperienceParagraph> January 2014 - </WorkExperienceParagraph>
-        <WorkExperienceParagraph>
+      <ItemContainer>
+        <Title>Work Experience</Title>
+        <LineThrough />
+        <Role>Front End Developer @ Hillson</Role>
+        <Paragraph> January 2014 - </Paragraph>
+        <Paragraph>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
           facilis nisi sapiente sit tempore earum maiores illo laudantium quae
           cum nobis, architecto eaque molestias autem rem, error voluptates,
           ipsum est!
-        </WorkExperienceParagraph>
-      </WorkExperienceItemContainer>
+        </Paragraph>
+      </ItemContainer>
 
-      <WorkExperienceItemContainer>
-        <WorkExperienceRole>Front End Developer @ Hillson</WorkExperienceRole>
-        <WorkExperienceParagraph> January 2014 - </WorkExperienceParagraph>
-        <WorkExperienceParagraph>
+      <ItemContainer>
+        <Role>Front End Developer @ Hillson</Role>
+        <Paragraph> January 2014 - </Paragraph>
+        <Paragraph>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
           facilis nisi sapiente sit tempore earum maiores illo laudantium quae
           cum nobis, architecto eaque molestias autem rem, error voluptates,
           ipsum est!
-        </WorkExperienceParagraph>
-      </WorkExperienceItemContainer>
+        </Paragraph>
+      </ItemContainer>
 
-      <WorkExperienceItemContainer>
-        <WorkExperienceRole>Front End Developer @ Hillson</WorkExperienceRole>
-        <WorkExperienceParagraph> January 2014 - </WorkExperienceParagraph>
-        <WorkExperienceParagraph>
+      <ItemContainer>
+        <Role>Front End Developer @ Hillson</Role>
+        <Paragraph> January 2014 - </Paragraph>
+        <Paragraph>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
           facilis nisi sapiente sit tempore earum maiores illo laudantium quae
           cum nobis, architecto eaque molestias autem rem, error voluptates,
           ipsum est!
-        </WorkExperienceParagraph>
-      </WorkExperienceItemContainer>
+        </Paragraph>
+      </ItemContainer>
     </div>
   );
 };
