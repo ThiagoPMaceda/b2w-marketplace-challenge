281930350030b3238bbd4d19f8c5f39607f00966
Thiago P <thiagopmaceda@gmail.com>
2020-04-14 19:59:04 -0300

    Removendo os arquivos de styled-components que agora estão na pasta de styles

--- src/components/Role.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Role = styled.h3`
-  text-transform: uppercase;
-  color: #000;
-`;
-
-export default Role;
--- src/components/Title.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Title = styled.h2`
-  text-transform: uppercase;
-  color: #000;
-`;
-
-export default Title;
