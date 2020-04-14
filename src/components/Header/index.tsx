b0b434f89d02604ac23426f3d1273206c69701b7
Thiago P <thiagopmaceda@gmail.com>
2020-04-14 20:13:01 -0300

    styled-components agora estao sendo pegos do styles.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const HeaderContainer = styled.div`
-  background-color: #333333;
-  display: flex;
-  height: 50vh;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-
-  @media (min-width: 600px) {
-    height: 1020px;
-  }
-
-  @media (min-width: 1024px) {
-    grid-column: 1;
-    grid-row: 1 / span 2;
-
-    height: 750px;
-    width: 420px;
-  }
-
-  @media (min-width: 1440px) {
-    grid-row: 1;
-    width: 100%;
-  }
-`;
-
-const HeaderImg = styled.img`
-  margin-bottom: 15%;
-`;
-
-const HeaderDevName = styled.h1`
-  color: white;
-  text-align: center;
-  letter-spacing: 10px;
-`;
-
-const HeaderDevRole = styled.p`
-  color: white;
-`;
+import {
+  HeaderContainer,
+  HeaderDevName,
+  HeaderDevRole,
+  HeaderImg,
+} from './styles';
 
 const Header = () => {
   return (
